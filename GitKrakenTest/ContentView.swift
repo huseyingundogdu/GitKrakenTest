a16fe569f7a5e9e078adae77d804b31a3145567e
Huseyin Gundogdu <73406964+huseyingundogdu@users.noreply.github.com>
2025-06-29 12:28:06 +0300

    Initial Commit

---
+++ GitKrakenTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitKrakenTest
+//
+//  Created by Hüseyin Gündoğdu on 29/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitKrakenTest/GitKrakenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitKrakenTestApp.swift
+//  GitKrakenTest
+//
+//  Created by Hüseyin Gündoğdu on 29/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitKrakenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
